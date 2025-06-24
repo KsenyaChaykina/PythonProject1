34b5c489a8a073a7fac41afe23a9932427133ea9
Ksenya Chaykina <ksushechka0901@gmail.com>
2025-06-25 02:51:26 +0300

    fixes, add docstring

--- src/processing.py
+++ src/processing.py
@@ -3,13 +3,14 @@ from typing import Iterable
 
 dict_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-
+"""Функция, возвращающая новый список словарей по заданному ключу"""
 def filter_by_state(dict_list: Iterable[str], state='EXECUTED') -> None:
     result = []
+    """Перебор значений по ключу"""
     for dict in dict_list:
         if dict.get('state') == state:
             result.append(dict)
-    print(result)
+    return result
 
 
 filter_by_state(dict_list)
@@ -17,10 +18,10 @@ filter_by_state(dict_list)
 
 dict_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-
+"""Функция, возвращающая новый список словарей, отсортированных по дате"""
 def sort_by_date(dict_list: Iterable[str]) -> None:
     sorted_list = sorted(dict_list, key=lambda dict: dict['date'], reverse=True)
-    print(sorted_list)
+    return sorted_list
 
 
 sort_by_date(dict_list)
