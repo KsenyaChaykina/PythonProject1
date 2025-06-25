bbdd5a23c039e32339dd352b32b9fda21dce26e2
Ksenya Chaykina <ksushechka0901@gmail.com>
2025-06-25 11:24:15 +0300

    fixes after check

--- src/processing.py
+++ src/processing.py
@@ -1,39 +1,27 @@
 from typing import Iterable
-from typing import Any
-import json
+
+
+dict_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
 """Функция, возвращающая новый список словарей по заданному ключу"""
-
-
-def filter_by_state(dictionarys_list: Iterable[list], state: str = "EXECUTED") -> list:
+def filter_by_state(dict_list: Iterable[str], state='EXECUTED') -> None:
     result = []
     """Перебор значений по ключу"""
-    for dictionary in dictionarys_list:
-        if dictionary.get('state') == state:
-            result.append(dictionary)
+    for dict in dict_list:
+        if dict.get('state') == state:
+            result.append(dict)
     return result
 
 
+filter_by_state(dict_list)
+
+
+dict_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
 """Функция, возвращающая новый список словарей, отсортированных по дате"""
-
-
-def sort_by_date(dictionarys_list: list) -> list:
-    sorted_list = sorted(dictionarys_list, key=lambda dictionarys_list: dictionarys_list['date'], reverse=True)
+def sort_by_date(dict_list: Iterable[str]) -> None:
+    sorted_list = sorted(dict_list, key=lambda dict: dict['date'], reverse=True)
     return sorted_list
 
 
-if __name__ == '__main__':
-    dictionarys_input = input()
-    # Заменяем одинарные кавычки на двойные
-    dictionarys_input = dictionarys_input.replace("'", '"')
-    # Преобразуем в JSON
-    dictionarys_list = json.loads(dictionarys_input)
-    # Вызов функции
-    print(filter_by_state(dictionarys_list))
-
-    dictionarys_input = input()
-    # Заменяем одинарные кавычки на двойные
-    dictionarys_input = dictionarys_input.replace("'", '"')
-    # Преобразуем в JSON
-    dictionarys_list = json.loads(dictionarys_input)
-    print(sort_by_date(dictionarys_list))
+sort_by_date(dict_list)
