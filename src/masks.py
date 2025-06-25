83a3fd359937c9d30ea8158df6412ef1d9fd83fc
Ksenya Chaykina <ksushechka0901@gmail.com>
2025-06-25 03:01:32 +0300

    fixes, add docstring

--- src/masks.py
+++ src/masks.py
@@ -1,45 +1,33 @@
-import os
-
-from config import ROOT_DIR
-from src.decorators import log
-
 """Маска номер карты"""
+card_num = input()
 
-@log()
+
 def get_mask_card_number(card_num: str) -> str:
     mask = "******"
     result = ""
     count = 0
     """Создание номера карты с маской"""
-    if len(card_num) >= 12:
-        card_num_str = str(card_num[:6] + mask + card_num[12:])
-        """Разделение номера карты для выведения по 4 символа через пробел"""
-        for i in card_num_str:
-            result += i
-            count += 1
-            if count % 4 == 0:
-                result += " "
-        result = result.strip()
-        return result
-    else:
-        return ''
+    card_num_str = str(card_num[:6] + mask + card_num[12:])
+    """Разделение номера карты для выведения по 4 символа через пробел"""
+    for i in card_num_str:
+        result += i
+        count += 1
+        if count % 4 == 0:
+            result += " "
+    return result
 
+
+get_mask_card_number(card_num)
 
 """Маска номер счета"""
+account_num = input()
 
-@log()
+
 def get_mask_account(account_num: str) -> str:
-    if len(account_num) >= 3:
-        mask = "**"
-        """Создание номера счета с маской"""
-        result = str(mask + account_num[-4:])
-        return result
-    else:
-        return ''
+    mask = "**"
+    """Создание номера счета с маской"""
+    result = str(mask + account_num[-4:])
+    return result
 
 
-if __name__ == '__main__':
-    card_num = input()
-    get_mask_card_number(card_num)
-    account_num = input()
-    get_mask_account(account_num)
+get_mask_account(account_num)
--- src/widget.py
+++ src/widget.py
@@ -1,42 +1,50 @@
-from src import masks
-
 """Обработка данных о картах и счетах"""
+user_input = input()
 
 
 def mask_account_card(user_input: str) -> str:
-    card_type = ""
-    replace_user_input = user_input.replace(" ", "")
-    if not replace_user_input.isdigit() and not replace_user_input.isalpha():
-        if 'Счет' in user_input:
-            """Создание номера счета с маской"""
-            account = user_input[-20:]
-            return 'Счет ' + masks.get_mask_account(account)
-        else:
-            for i in user_input:
-                if i.isalpha() or i.isspace():
-                    card_type += i
-            number_card = "".join(user_input[-16:].split())
-            return card_type + masks.get_mask_card_number(number_card)
+    if user_input.find('Счет') != -1:
+        mask = "**"
+        """Создание номера счета с маской"""
+        result = "Счет " + str(mask + user_input[-4:])
+        return result
     else:
-        return 'Ошибка ввода данных'
+        mask = "******"
+        result = ""
+        count = 0
+        card_type = ""
+        """Оставляет только тип карты, без номера карты"""
+        for i in user_input:
+            if i.isalpha() or i.isspace():
+                card_type += i
+        """Оставляет только номер карты, без типа карты"""
+        curd_num = ''.join(filter(str.isdigit, user_input))
+        """Создание номера карты с маской"""
+        card_num_str = str(curd_num[:6] + mask + curd_num[12:])
+        """Разделение номера карты для выведения по 4 символа через пробел"""
+        for i in card_num_str:
+            result += i
+            count += 1
+            if count % 4 == 0:
+                result += " "
+        return f"{card_type}" + result
+
+
+mask_account_card(user_input)
 
 
 """Обработка даты"""
+user_input = input()
 
 
-def get_date(user_date: str) -> str:
+def get_date(user_input: str) -> str:
+    """Вычисляем значения день, месяц, год"""
+    day = user_input[8:10]
+    month = user_input[5:7]
+    year = user_input[:4]
     """Соединяем день, месяц и год в дату"""
-    times = user_date[:user_date.find("T")].split("-")
-    reversed_times = times[::-1]
-    if len(times) < 2:
-        return 'Ошибка ввода данных'
-    else:
-        return ".".join(reversed_times)
+    date = ".".join([day, month, year])
+    return date
 
 
-if __name__ == "__main__":
-    user_input = input()
-    print(mask_account_card(user_input))
-    """Обработка даты"""
-    user_date = input()
-    get_date(user_date)
+get_date(user_input)
