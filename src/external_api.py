33dbffc920fb08d9ac18882e7c07f85691642a29
Ksenya Chaykina <ksushechka0901@gmail.com>
2025-10-04 15:14:18 +0300

    add: operations.json, .env.example, external_api.py, utils.py, test_external_api.py

---
+++ src/external_api.py
@@ -0,0 +1,37 @@
+import os
+import json
+from dotenv import load_dotenv
+from src.utils import financial_transactions
+import requests
+
+"""Загрузка переменных их файла env"""
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+
+def transaction_amount(transaction: dict) -> float:
+    if transaction['operationAmount']['currency']['code'] == 'RUB':
+        return float(transaction['operationAmount']['amount'])
+    try:
+        my_amount = transaction['operationAmount']['amount']
+        my_from = transaction['operationAmount']['currency']['code']
+        my_to = "RUB"
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={my_to}&from={my_from}&amount={my_amount}"
+        headers = {
+            "apikey": API_KEY
+        }
+
+        response = requests.get(url, headers=headers)
+        status_code = response.status_code
+
+        return float(response.json()['result'])
+        # Обработка ошибки ключа
+    except KeyError:
+        return 0
+
+
+if __name__ == '__main__':
+    transactions = financial_transactions('C:\\Users\\Agilent_2022\\PycharmProjects\\PythonProject1\\data\\operations.json')
+    for transaction in transactions:
+        print(transaction_amount(transaction))
---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+
+"""Возвращает список словарей с данными о финансовых транзакциях"""
+
+
+def financial_transactions(file_path: str) -> list[dict]:
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data_list = json.load(f)
+            return data_list
+        """Обработка ошибок"""
+    except FileNotFoundError:
+        return []
+    except Exception as e:
+        return []
+
+
+if __name__ == '__main__':
+    print(financial_transactions('C:\\Users\\Agilent_2022\\PycharmProjects\\PythonProject1\\data\\operations.json'))
