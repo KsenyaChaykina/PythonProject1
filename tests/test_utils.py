52187086217b518aa1871ec642cfda7583f80e75
Ksenya Chaykina <ksushechka0901@gmail.com>
2025-10-05 18:56:13 +0300

    add: test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,9 +5,18 @@ from src.utils import financial_transactions
 class TestFinancialTransactions(unittest.TestCase):
     def test_financial_transactions(self):
         mock_data = '{"key": "value"}'
-        with patch('builtins.open', mock_open(read_data=mock_data)):
-            result = financial_transactions('dummy_path.json')
-            self.assertEqual(result, {"key": "value"})
+        try:
+            with patch('builtins.open', mock_open(read_data=mock_data)):
+                result = financial_transactions('dummy_path.json')
+                self.assertEqual(result, {"key": "value"})
+                self.assertIsInstance(result, list)
+        except FileNotFoundError:
+            self.assertRaises(FileNotFoundError)
+        except Exception:
+            self.assertRaises(Exception)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
